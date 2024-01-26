b49b9344657c7dbb5a68f2454741b7fb01c11a9a
revand5467 <revandsunil5467@gmail.com>
2024-01-26 23:27:56 +0530

    commit

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import '../../styles/globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+      <title>Codesters</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/blockchain.png" />
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      </head>
+      <body className={inter.className}>{
+      
+      children}</body>
     </html>
   );
 }
